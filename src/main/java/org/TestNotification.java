9aeb3f3624c351f22ef4628b31e1e976a0601f4d
chriszhou770 <63235476+chriszhou770@users.noreply.github.com>
2025-06-01 18:06:45 +0800

    Update TestNotification.java

--- src/main/java/org/TestNotification.java
+++ src/main/java/org/TestNotification.java
@@ -1,24 +1,14 @@
-import javafx.animation.*;
-import javafx.application.Application;
+package org;
+
+
+
 import javafx.application.Platform;
-import javafx.beans.value.WritableValue;
-import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.util.Duration;
 
 public class TestNotification {
     public static void main(String[] args) {
-        CrossPlatformNotification.showNotification("Hello! This is a test .");
+        // 啟動JavaFX Platform
+        Platform.startup(() -> {
+            CrossPlatformNotification.show("讚讚");
+        });
     }
 }
